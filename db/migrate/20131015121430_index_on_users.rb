599f6ae42e75038b25952eecfe4f0c80fb338cd0
Markus Kahl <machisuji@googlemail.com>
2013-12-11 11:54:03 +0100

    reduce index size to make migration work

--- db/migrate/20131015121430_index_on_users.rb
+++ db/migrate/20131015121430_index_on_users.rb
@@ -1,6 +1,17 @@
+#-- copyright
+# OpenProject is a project management system.
+#
+# Copyright (C) 2012-2013 the OpenProject Foundation (OPF)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# See doc/COPYRIGHT.rdoc for more details.
+#++
+
 class IndexOnUsers < ActiveRecord::Migration
   def change
-    add_index :users, [:type, :login], :length => {:type => 255, :login => 255}
+    add_index :users, [:type, :login], :length => {:type => 128, :login => 128}
     add_index :users, [:type, :status]
   end
 end
