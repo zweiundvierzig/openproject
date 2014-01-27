bf2f7fae9323dab0832e779c9708a1a4fd1578ef
Markus Kahl <machisuji@googlemail.com>
2014-01-27 17:22:03 +0000

    don't drop journals as they are still used

--- db/migrate/20140127142157_remove_obsolete_tables_and_columns.rb
+++ db/migrate/20140127142157_remove_obsolete_tables_and_columns.rb
@@ -2,7 +2,6 @@
 # Removes obsolete tables and columns that, for whatever reason, were not deleted before.
 class RemoveObsoleteTablesAndColumns < ActiveRecord::Migration
   def up
-    drop_table :journals if ActiveRecord::Base.connection.table_exists? 'legacy_journals'
     drop_table :principal_roles if ActiveRecord::Base.connection.table_exists? 'principal_roles'
 
     remove_column :repositories, :checkout_settings
